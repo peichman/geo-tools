48a60364467f95c3f506c84b181818748e22f2ac
Peter Eichman <peichman@cpan.org>
2021-08-31 23:15:25 -0400

    Generalize tabulate.py to calculate other stats.
    Stats available are distance, elevation, clock_time, and elapsed_time.

--- tabulate.py
+++ tabulate.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env python3
 
 import sys
+from datetime import timedelta
 
 import click
 import gpxpy.gpx
-
 from geographiclib.geodesic import Geodesic
-
-
-geod = Geodesic.WGS84
 
 
 @click.command()
 @click.argument('gpx_file', type=click.File('r'), default=sys.stdin)
 def main(gpx_file):
     gpx = gpxpy.parse(gpx_file)
+    fields = (distance, elevation)
 
-    total_meters = 0
     for track in gpx.tracks:
         for segment in track.segments:
             for n, point in enumerate(segment.points[1:], 1):
-                # get distance between the current point and previous point
-                previous_point = segment.points[n - 1]
-                g = geod.Inverse(
-                    previous_point.latitude, previous_point.longitude,
-                    point.latitude, point.longitude,
-                    Geodesic.DISTANCE
-                )
-                distance = g['s12']
-                total_meters += distance
-                print(total_meters, point.elevation)
+                print(*(fn(n, point, segment) for fn in fields))
+
+
+# track a total (or other static value) as a function attribute
+# this is preserved across calls
+def track(attr_name, init=0):
+    def decorated(func):
+        if callable(init):
+            setattr(func, attr_name, init())
+        else:
+            setattr(func, attr_name, init)
+        return func
+    return decorated
+
+
+@track('total')
+def distance(n, point, segment):
+    # get distance between the current point and previous point
+    previous_point = segment.points[n - 1]
+    g = Geodesic.WGS84.Inverse(
+        previous_point.latitude, previous_point.longitude,
+        point.latitude, point.longitude,
+        Geodesic.DISTANCE
+    )
+    distance.total += g['s12']
+    return distance.total
+
+
+def elevation(n, point, segment):
+    return point.elevation
+
+
+def clock_time(n, point, segment):
+    return point.time.isoformat()
+
+
+@track('total', init=timedelta)
+def elapsed_time(n, point, segment):
+    previous_point = segment.points[n - 1]
+    elapsed_time.total += (point.time - previous_point.time)
+    return elapsed_time.total
 
 
 if __name__ == '__main__':
